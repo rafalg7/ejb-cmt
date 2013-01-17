4b37e888745c22f852a261cff9e986d053993086
Rafal Gielczowski <rafal.gielczowski@itcrowd.pl>
2013-01-17 11:04:14 +0100

    Added MySingleton @Startup @Singleton class

---
+++ src/main/java/pl/itcrowd/tutorials/ejbcmt/singletons/MySingleton.java
@@ -0,0 +1,41 @@
+package pl.itcrowd.tutorials.ejbcmt.singletons;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.TransactionSynchronizationRegistry;
+import java.util.logging.Logger;
+
+@Startup
+@Singleton
+public class MySingleton {
+
+    private static final Logger LOGGER = Logger.getLogger(MySingleton.class.getCanonicalName());
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Resource
+    private TransactionSynchronizationRegistry registry;
+
+    @EJB
+    private FirstService firstService;
+
+    @EJB
+    private SecondService secondService;
+
+    public MySingleton() {
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    @PostConstruct
+    public void onCreate(){
+        LOGGER.info("TX "+registry.getTransactionKey().toString()+" status before:" + registry.getTransactionStatus());
+        firstService.firstBusinessMethod();
+        LOGGER.info("TX "+registry.getTransactionKey().toString()+" status after first service:" + registry.getTransactionStatus());
+        secondService.secondBusinessMethod();
+        LOGGER.info("TX "+registry.getTransactionKey().toString()+" status after second service:" + registry.getTransactionStatus());
+    }
+}
